b3aaa4aa6c47112b904d638b007da22605967ad8
EmmanuelU <emmanuelurias60@icloud.com>
2022-10-15 14:35:47 -0700

    finished

--- index.js
+++ index.js
@@ -2,37 +2,111 @@ let header = document.createElement('h1')
 header.textContent = 'Warranty Information'
 document.body.append(header)
 
+function warrantyPage() {
 let firstNameLabel = document.createElement('label')
 firstNameLabel.textContent = 'First Name'
-firstNameInput.style.display = 'block'
 document.body.append(firstNameLabel)
 let firstNameInput = document.createElement('input')
+firstNameInput.style.display = 'block'
 document.body.append(firstNameInput)
 
 let lastNameLabel = document.createElement('label')
-lastNameLabel.textContent = 'Warranty Information'
-lastNameInput.style.display = 'block'
+lastNameLabel.textContent = 'Last Name'
 document.body.append(lastNameLabel)
 let lastNameInput = document.createElement('input')
+lastNameInput.style.display = 'block'
 document.body.append(lastNameInput)
 
 let companyLabel = document.createElement('label')
-companyLabel.textContent = 'Warranty Information'
-companyInput.style.display = 'block'
+companyLabel.textContent = 'Company'
 document.body.append(companyLabel)
 let companyInput = document.createElement('input')
+companyInput.style.display = 'block'
 document.body.append(companyInput)
 
 let emailLabel = document.createElement('label')
-emailLabel.textContent = 'Warranty Information'
-emailInput.style.display = 'block'
+emailLabel.textContent = 'Email'
 document.body.append(emailLabel)
 let emailInput = document.createElement('input')
+emailInput.style.display = 'block'
 document.body.append(emailInput)
 
 let phoneNumberLabel = document.createElement('label')
-phoneNumberLabel.textContent = 'Warranty Information'
-phoneNumberInput.style.display = 'block'
+phoneNumberLabel.textContent = 'Phone Number'
 document.body.append(phoneNumberLabel)
 let phoneNumberInput = document.createElement('input')
+phoneNumberInput.style.display = 'block'
 document.body.append(phoneNumberInput)
+
+let faxNumberLabel = document.createElement('label')
+faxNumberLabel.textContent = 'Fax Number'
+document.body.append(faxNumberLabel)
+let faxNumberInput = document.createElement('input')
+faxNumberInput.style.display = 'block'
+document.body.append(faxNumberInput)
+
+let addressLabel = document.createElement('label')
+addressLabel.textContent = 'Address'
+document.body.append(addressLabel)
+let addressInput = document.createElement('input')
+addressInput.style.display = 'block'
+document.body.append(addressInput)
+
+let cityLabel = document.createElement('label')
+cityLabel.textContent = 'City'
+document.body.append(cityLabel)
+let cityInput = document.createElement('input')
+cityInput.style.display = 'block'
+document.body.append(cityInput)
+
+let stateLabel = document.createElement('label')
+stateLabel.textContent = 'State'
+document.body.append(stateLabel)
+let stateInput = document.createElement('input')
+stateInput.style.display = 'block'
+document.body.append(stateInput)
+
+let zipLabel = document.createElement('label')
+zipLabel.textContent = 'Zip'
+document.body.append(zipLabel)
+let zipInput = document.createElement('input')
+zipInput.style.display = 'block'
+document.body.append(zipInput)
+
+let countryLabel = document.createElement('label')
+countryLabel.textContent = 'Country'
+document.body.append(countryLabel)
+let countryInput = document.createElement('input')
+countryInput.style.display = 'block'
+document.body.append(countryInput)
+
+let unitInformationLabel = document.createElement('label')
+unitInformationLabel.textContent = 'Unit Information'
+document.body.append(unitInformationLabel)
+let unitInformationInput = document.createElement('input')
+unitInformationInput.style.display = 'block'
+document.body.append(unitInformationInput)
+
+let dateOfPurchaseLabel = document.createElement('label')
+dateOfPurchaseLabel.textContent = 'Date Of Purchase'
+document.body.append(dateOfPurchaseLabel)
+let dateOfPurchaseInput = document.createElement('input')
+dateOfPurchaseInput.style.display = 'block'
+document.body.append(dateOfPurchaseInput)
+
+let modelLabel = document.createElement('label')
+modelLabel.textContent = 'Model'
+document.body.append(modelLabel)
+let modelInput = document.createElement('input')
+modelInput.style.display = 'block'
+document.body.append(modelInput)
+
+let warrantySerialNumberLabel = document.createElement('label')
+warrantySerialNumberLabel.textContent = 'Warranty Serial Number'
+document.body.append(warrantySerialNumberLabel)
+let warrantySerialNumberInput = document.createElement('input')
+warrantySerialNumberInput.style.display = 'block'
+document.body.append(warrantySerialNumberInput)
+}
+
+warrantyPage()
