99a995830300e73527533493d6502732b544cf4a
EmmanuelU <emmanuelurias60@icloud.com>
2022-10-15 13:52:36 -0700

    webpage

---
+++ index.js
@@ -0,0 +1,38 @@
+let header = document.createElement('h1')
+header.textContent = 'Warranty Information'
+document.body.append(header)
+
+let firstNameLabel = document.createElement('label')
+firstNameLabel.textContent = 'First Name'
+firstNameInput.style.display = 'block'
+document.body.append(firstNameLabel)
+let firstNameInput = document.createElement('input')
+document.body.append(firstNameInput)
+
+let lastNameLabel = document.createElement('label')
+lastNameLabel.textContent = 'Warranty Information'
+lastNameInput.style.display = 'block'
+document.body.append(lastNameLabel)
+let lastNameInput = document.createElement('input')
+document.body.append(lastNameInput)
+
+let companyLabel = document.createElement('label')
+companyLabel.textContent = 'Warranty Information'
+companyInput.style.display = 'block'
+document.body.append(companyLabel)
+let companyInput = document.createElement('input')
+document.body.append(companyInput)
+
+let emailLabel = document.createElement('label')
+emailLabel.textContent = 'Warranty Information'
+emailInput.style.display = 'block'
+document.body.append(emailLabel)
+let emailInput = document.createElement('input')
+document.body.append(emailInput)
+
+let phoneNumberLabel = document.createElement('label')
+phoneNumberLabel.textContent = 'Warranty Information'
+phoneNumberInput.style.display = 'block'
+document.body.append(phoneNumberLabel)
+let phoneNumberInput = document.createElement('input')
+document.body.append(phoneNumberInput)
